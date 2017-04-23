00bb09d2363062043430a0e2e3aad95c41f451ca
Karthik Sreekumar <kskarthiksk@gmail.com>
2017-04-23 11:07:23 +0000

    Implemented counting sort

--- helpers.c
+++ helpers.c
@@ -7,15 +7,14 @@
 #include <cs50.h>
 
 #include "helpers.h"
-
+#define MAX 67736
 /**
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
     int L=0,U=n,mid;
-    if(n<0)
+    if(n<=0)
         return false;
     while(L<=U)
     {
@@ -36,5 +35,21 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    int ar[65536],i,j=0;
+    for(i=0;i<MAX;i++)
+        ar[i]=0;
+    for(i=0;i<n;i++)
+        ar[values[i]]++;
+    for(i=0;i<n&&j<MAX;)
+    {
+        if(ar[j]>0)
+        {
+            values[i]=ar[j];
+            i++;
+            j--;
+        }
+        else
+            j++;
+    }
     return;
 }
